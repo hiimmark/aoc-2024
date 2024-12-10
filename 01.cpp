20838ae1b497c7814d1fa41758e457613b29e263
Mark Ma <76633497+hiimmark@users.noreply.github.com>
2024-12-10 09:36:49 -0500

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "/Users/envyaims/Documents/template/debug.cpp"
+#endif
+using namespace std;
+using ll = long long;
+
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define pq priority_queue
+#define FOR(i,a,b) for(int i = (a); i < (b); ++i)
+#define FORE(i,a,b) for(int i = (a); i <= (b); ++i)
+#define ROF(i,a,b) for(int i = (a); i >= (b); --i)
+#define trav(a,x) for(auto& a: x)
+#define sz(x) (int)x.size()
+#define make_unique(v) v.erase(unique(all(v)), v.end());
+ 
+template<class T> using minpq = pq<T, vector<T>, greater<T>>;
+template<class T> bool ckmin(T& a, const T& b){return b<a?a=b,1:0;}
+template<class T> bool ckmax(T& a, const T& b){return a<b?a=b,1:0;}
+template<int D, typename T>struct vt : public vector<vt<D - 1, T>> { template<typename... Args>
+	vt(int n = 0, Args... args) : vector<vt<D - 1, T>>(n, vt<D - 1, T>(args...)) {}};
+template<typename T> struct vt<1, T> : public vector<T> {
+	vt(int n = 0, const T& val = T()) : vector<T>(n, val) {}};
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T> ostream& operator<<(ostream& out, vector<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+
+#define F0R(i, b) for(int i = 0; i < (b); ++i)
+
+void uwu(){
+	vector<int> a, b;
+	while(1){
+		if(cin.eof()) break;
+		int A, B; cin >> A >> B;
+		a.pb(A); b.pb(B);
+	}
+	map<ll, ll> mp;
+	trav(i, b) mp[i]++;
+	ll ans = 0;
+	trav(i, a) ans += i * mp[i];
+	cout << ans << endl;
+}
+
+signed main(){
+	cin.tie(0) -> sync_with_stdio(0);
+	int t = 1;
+	// cin>>t;
+	while(t--){
+		uwu();
+	}
+}
